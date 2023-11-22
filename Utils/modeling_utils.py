d9137190753132bb8b073058a4f1890dc571e4e2
Dacarpe03 <d.carmonap97@gmail.com>
2023-11-22 14:52:35 +1100

    Add create_linear_model to modeling_utils

---
+++ Utils/modeling_utils.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+from tensorflow import keras
+
+from keras.models import Sequential
+from keras.layers import InputLayer, Conv2D, MaxPooling2D, Flatten, Dense, BatchNormalization, Activation
+
+def create_linear_architecture(
+	input_shape,
+	output_size,
+	hidden_layer_sizes,
+	regularizer,
+	hidden_activation,
+	output_activation,
+	use_batch_normalization=True,
+	name="SurfaceReconstructor"):
+
+	"""
+	Defines de architecture of the neural network
+
+	Input:
+		input_shape (tuple): The shape a data point in the features dataset
+		output_size (int): The length of a data point in the labels dataset
+		hidden_layer_sizes (list): A list of integers
+		regularizer (keras.regularizers): A regularizer for the hidden layers (e.g. L1, see keras documentation for more)
+		hidden_activation (string): The name of the activation function of the hidden layers' neurons  (e.g 'relu', see keras documentation for more)
+		output_activation (string): The name of the activation function of the output layers (e.g 'linear', see keras documentation for more)
+		use_batch_normalization (bool): If True, then add batch normalization to the hidder layers
+		name (string): The name of the model
+
+	Returns:
+		model (keras.Sequential): A keras neural network model with the architecture specified
+	"""
+
+	# Create a sequential model
+	model = Sequential(
+		name=name
+		)
+
+	# Create input layer
+	model.add(
+		InputLayer(
+			input_shape=input_shape,
+			batch_size=None)
+			)
+
+	# Create the hidden layers of the neural network
+	for neurons in hidden_layer_sizes:
+
+		# Define layer
+		model.add(
+			Dense(
+				neurons,
+				kernel_regularizer=regularizer,
+				kernel_initializer=keras.initializers.HeNormal(seed=None),
+				use_bias=False
+				)
+			)
+
+		# Add normalization
+		if use_batch_normalization:
+			model.add(
+				BatchNormalization()
+				)
+
+		# Define the activation function
+		model.add(
+			Activation(
+				hidden_activation
+				)
+			)
+
+	# Add output layer
+	model.add(
+		Dense(
+			output_size,
+			activation=output_activation
+			)
+		)
+
+	return model
