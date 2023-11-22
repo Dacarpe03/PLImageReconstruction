b895b743482d2ebaf319093c4280eedde5e053a9
Dacarpe03 <d.carmonap97@gmail.com>
2023-11-22 16:08:19 +1100

    Add train function for linear model

--- Utils/modeling_utils.py
+++ Utils/modeling_utils.py
@@ -3,18 +3,18 @@ import tensorflow as tf
 from tensorflow import keras
 
 from keras.models import Sequential
-from keras.layers import InputLayer, Conv2D, MaxPooling2D, Flatten, Dense, BatchNormalization, Activation
+from keras.layers import InputLayer, Conv2D, MaxPooling2D, Flatten, Dense, BatchNormalization, Activation, Reshape
 
 
 def create_linear_architecture_for_amplitude_reconstruction(
 	input_shape,
-	output_size,
+	output_shape,
 	hidden_layer_sizes,
 	regularizer,
 	hidden_activation,
 	output_activation,
 	use_batch_normalization=True,
-	name="SurfaceReconstructor"
+	name="AmplitudeReconstructor"
 	):
 	"""
 	Defines de architecture of the neural network
@@ -32,7 +32,7 @@ def create_linear_architecture_for_amplitude_reconstruction(
 	Returns:
 		model (keras.Sequential): A keras neural network model with the architecture specified
 	"""
-	# As the output is an image, we will show the fucking
+	# As the output is an image, we need to create a final layer with as many neurons as pixels
 	output_size = output_shape[0] * output_shape[1]
 	# Create a sequential model
 	model = Sequential(
@@ -88,3 +88,67 @@ def create_linear_architecture_for_amplitude_reconstruction(
 		)
 
 	return model
+
+
+def compile_linear_model_for_amplitude_reconstruction(
+	model,
+	loss_function,
+	optimizer,
+	metric
+	):
+	"""
+	Tells the model how to train
+
+	Input:
+		model(keras.Sequential): The sequential model to compile
+		loss_function (keras.losses): The loss function used to update the weights of the neurons (eg. MeanSquaredError())
+		optimizer (keras.optimizers): The optimizer used to update the weights of the neurons (eg. Adam)
+		metric (keras.metrics): The metrics to monitor during the training (eg. MeanSquaredError())
+
+	Returns:
+		None
+	"""
+	model.compile(
+		loss=loss_function,
+		optimizer=optimizer,
+		metrics=[metric]
+		)
+
+	return None
+
+
+def train_linear_model_for_amplitude_reconstruction(
+	model,
+	train_features,
+	train_labels,
+	batch_size,
+	epochs,
+	val_features,
+	val_labels,
+	callbacks):
+
+	"""
+	Fits the model to the train instances of the data.
+
+	Input:
+		model (keras.Sequential): The sequential model to train
+		train_features (np.array): An np.array containing np.array with the train features
+		train_labels (np.array): An np.array containing np.array with the train features
+		batch_size(int): The batch size of training samples used before each weight update
+		epochs (int): The number of times the training goes through the training data
+		val_features (np.array): An np.array containing np.array with the train features
+		val_labels (np.array): An np.array containing np.array with the train features 
+		callbacks (list): A list of keras callbacks used during the training.
+
+	Returns:
+		history (): The training history of the model
+	"""
+	history = model.fit(train_features,
+						train_labels,
+						batch_size=batch_size,
+						epochs=epochs,
+						validation_data=(val_features, val_labels),
+						callbacks=callbacks,
+						verbose=1)
+
+	return history
