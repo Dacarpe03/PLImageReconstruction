3723200e3e0fe2c339fe0abfb5b56ae06db7cc6b
Dacarpe03 <d.carmonap97@gmail.com>
2023-11-22 13:34:54 +1100

    Add function to load numpy data

---
+++ Utils/data_utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def load_numpy_data(data_filepath,
+			  		n_points=None):
+	"""
+	Function that retrieves numpy data given a path
+
+	Input:
+		data_filepath (string): The data file path
+		n_points (int): The number of points to load
+
+	Returns:
+		data (np.array): An array containing the loaded data
+	"""
+	if n_points is not None:
+		data = np.load_data(data_filepath)[0:n_points]
+	else:
+		data = np.load_data(data_filepath)
+
+	return data
