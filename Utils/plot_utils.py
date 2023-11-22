4a0531916210a00df9c902dcc9b35bbbcb05e976
Dacarpe03 <d.carmonap97@gmail.com>
2023-11-22 14:33:39 +1100

    Add plot_model_history function

--- Utils/plot_utils.py
+++ Utils/plot_utils.py
@@ -1,7 +1,11 @@
 import plotly.express as px
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def plot_map(whatever_map):
+def plot_map(
+	whatever_map
+	):
 	"""
 	Plots an amplitude, phase of flux map
 
@@ -11,7 +15,31 @@ def plot_map(whatever_map):
 	Returns:
 		None
 	"""
-	
+
 	fig = px.imshow(whatever_map)
 	fig.show()
 	return None
+
+
+def plot_model_history(
+    history
+    ):
+    """
+    Plots the history of the model in a graph
+
+    Input:
+        history (): The training history of the model
+
+    Returns:
+        None
+    """
+    results = pd.DataFrame(history.history)
+    results.plot(figsize=(8,5))
+    plt.grid(True)
+    plt.xlabel('Epochs')
+    plt.ylabel('Mean Squared Error')
+    plt.ylim(top=0.5, bottom=0)
+    
+    plt.show()
+
+    return None
