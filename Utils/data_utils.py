d6705a99e6f3f0a2e16c579a43682b8ee4b9e8a5
Dacarpe03 <d.carmonap97@gmail.com>
2023-11-22 14:43:22 +1100

    Add flattend data function

--- Utils/data_utils.py
+++ Utils/data_utils.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 
-def load_numpy_data(data_filepath,
-			  		n_points=None):
+def load_numpy_data(
+	data_filepath,
+	n_points=None
+	):
 	"""
 	Function that retrieves numpy data given a path
 
@@ -20,3 +22,24 @@ def load_numpy_data(data_filepath,
 		data = np.load(data_filepath)
 
 	return data
+
+
+def flatten_data(
+	data
+	):
+	"""
+	Function that flattens an array of 2d arrays into an array of 1d arrays
+
+	Input:
+		data (np.array): The 3d array to flatten
+
+	Returns:
+		data (np.array): The 2d array with flattened matrixes into vectors
+	"""
+
+	n_points, x_length, y_length = data.shape
+	flat_length = x_length * y_length
+
+	flattened_data = data.reshape(n_points, flat_length)
+
+	return flattened_data
