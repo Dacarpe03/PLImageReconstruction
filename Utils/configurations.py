1e874b72f8f16a9989dedb126bda7d71eb9ee9ca
Dacarpe03 <d.carmonap97@gmail.com>
2023-11-27 11:44:39 +1100

    Create new file to store configurations

---
+++ Utils/configurations.py
@@ -0,0 +1,16 @@
+class FullyConnectedArchitecture(object):
+	"""This class contains the information of the architecture of a model"""
+	def __init__(
+		self, 
+		input_shape,
+		output_shape,
+		hidden_layer_sizes,
+		regularizer,
+		hidden_activation,
+		output_activation,
+		use_batch_normalization,
+		model_name
+		):
+		super(ClassName, self).__init__()
+		self.arg = arg
+		
