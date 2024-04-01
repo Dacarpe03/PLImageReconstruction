2b112a422cc662f24a318407f6fc962592c6904a
Dacarpe03 <d.carmonap97@gmail.com>
2024-04-02 08:03:51 +1100

    Fix import

--- PSFReconstruction/Scripts/TrainCroppedDR02FC10000.py
+++ PSFReconstruction/Scripts/TrainCroppedDR02FC10000.py
@@ -21,12 +21,13 @@ from kobol_configurations import CroppedDR02FC as ModelConfig
 from plot_utils import plot_amplitude_phase_fully_connected_prediction_from_electric_field, \
                        plot_model_history
 
+
+import tensorflow as tf
+ 
 gpus = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
 
-import tensorflow as tf
- 
 validation_fluxes_array = load_numpy_data(FC_PROCESSED_VALIDATION_OUTPUT_FLUXES_FILE_PATH)
 validation_complex_fields_array = load_numpy_data(FC_CROPPED_VALIDATION_COMPLEX_FIELDS_FILE_PATH)
 
