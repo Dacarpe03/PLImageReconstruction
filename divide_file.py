7c212c3e897806b9c2873c543affeb0a9fa07b58
Dacarpe03 <d.carmonap97@gmail.com>
2024-05-28 10:54:41 +1000

    Remove ld files

---
+++ divide_file.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+names = [
+"ld_20M_zernike_complex_fields.npy.npy",
+"ld_14M_zernike_complex_fields.npy.npy",
+"ld_9M_zernike_complex_fields.npy.npy",
+"ld_5M_zernike_complex_fields.npy.npy",
+"ld_2M_zernike_complex_fields.npy.npy"
+]
+
+for name in names:
+	vector = np.load(name)
+
+	start = 0
+	end = 10000
+	jump = 10000
+	for i in range(7):
+		new_name = name.replace(".npy.npy", f"_{i}.npy")
+		new_vector = vector[start:end]
+		print(new_vector.shape)
+		np.save(new_name, new_vector)
+		start += jump
+		end += jump
